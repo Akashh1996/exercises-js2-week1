e7d80be5623682dea70ad6c3a09e0d7af3e4ccc3
Akash <akashh.sapkota@gmail.com>
2020-04-28 03:39:39 +0200

    creating object method that accepts parameter to push in an array inside object

--- homework/exercise-2.js
+++ homework/exercise-2.js
@@ -6,7 +6,10 @@ Define a method "makeFriend" to add a new friend to her list.
 
 var person = {
     name: "Alice",
-    friends: ["John", "Nina"]
+    friends: ["John", "Nina"],
+    makeFriend : function (name){
+         this.friends.push(name)
+    }
 };
 
 
@@ -15,5 +18,9 @@ DO NOT EDIT ANYTHING BELOW THIS LINE
 */
 
 person.makeFriend("Bob");
+person.makeFriend("akash");
+
 
 console.log("Expected result: 'John,Nina,Bob'. Actual result: " + person.friends);
+
+console.log(person);
